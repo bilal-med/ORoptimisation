50ede6c083b4446c124f8c66f7578e7359ef8e97
saidabde <abdennacer.saidi@grenoble-inp.fr>
2023-06-26 02:56:23 +0200

    sz

--- CAR_kinematic_model/utils.py
+++ CAR_kinematic_model/utils.py
@@ -7,12 +7,21 @@ import matplotlib.font_manager as font_manager
 def angle_of_line(x1, y1, x2, y2):
     return math.atan2(y2-y1, x2-x1)
 
+import numpy as np
+
 def make_square(x, y, width):
-    square = np.array([[x-int(width/2),i] for i in range(y-int(width/2),y+int(width/2))] +\
-                      [[x+int(width/2),i] for i in range(y-int(width/2),y+int(width/2))] +\
-                      [[i,y-int(width/2)] for i in range(x-int(width/2),x+int(width/2))] +\
-                      [[i,y+int(width/2)] for i in range(x-int(width/2),x+int(width/2))]) 
+    outline = np.array([[x - int(width / 2), i] for i in range(y - int(width / 2), y + int(width / 2))] +
+                      [[x + int(width / 2), i] for i in range(y - int(width / 2), y + int(width / 2))] +
+                      [[i, y - int(width / 2)] for i in range(x - int(width / 2), x + int(width / 2))] +
+                      [[i, y + int(width / 2)] for i in range(x - int(width / 2), x + int(width / 2))])
+
+    interior = np.array([[i, j] for i in range(x - int(width / 2) + 1, x + int(width / 2)) for j in
+                         range(y - int(width / 2) + 1, y + int(width / 2))])
+
+    square = np.concatenate((outline, interior))
+
     return square
+
 
 class DataLogger:
     def __init__(self):
